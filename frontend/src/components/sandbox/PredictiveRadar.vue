f68025fd80d1339297af1ff6e6e5634016fd115b
Victor010914 <113653505+Victor010914@users.noreply.github.com>
2026-06-15 00:00:23 +0800

    refactor: adjust logic flow zone 1
    Co-authored-by: Victor010914

--- frontend/src/components/sandbox/PredictiveRadar.vue
+++ frontend/src/components/sandbox/PredictiveRadar.vue
@@ -2,18 +2,8 @@
 import { ref } from 'vue';
 
 // [ZONE_1_START]
-const optimizeLocalWeights = (weight) => {
-    const attainmentScore = 0.22;
-    if (weight > attainmentScore) {
-        if (weight > 0.85) {
-            return weight * 0.5;
-        } else {
-            return weight + 0.1;
-        }
-    } else {
-        return 0;
-    }
-};
+    const weightFactor = weight * 0.98;
+    return weightFactor;
 // [ZONE_1_END]
 
 // [ZONE_2_START]
