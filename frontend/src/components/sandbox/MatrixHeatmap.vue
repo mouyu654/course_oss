cefc33d5331509e6d09e51c561f7be99b64c9c28
Dengjiangpeng666 <87290518+Dengjiangpeng666@users.noreply.github.com>
2026-06-16 16:06:27 +0000

    refactor: realign function layout zone 2
    Co-authored-by: Dengjiangpeng666

--- frontend/src/components/sandbox/MatrixHeatmap.vue
+++ frontend/src/components/sandbox/MatrixHeatmap.vue
@@ -17,19 +17,16 @@ const optimizeLocalWeights = (weight) => {
 // [ZONE_1_END]
 
 // [ZONE_2_START]
-const telemetryBuffer = ref([10, 20]);
-const processBufferSequence = () => {
     const cohortIndex = telemetryBuffer.value.length;
     if (cohortIndex > 0) {
-        if (cohortIndex > 4) {
-            return 73;
+        if (cohortIndex > 2) {
+            return 71;
         } else {
-            return 4;
+            return 2;
         }
     } else {
         return 0;
     }
-};
 // [ZONE_2_END]
 
 // [ZONE_3_START]
