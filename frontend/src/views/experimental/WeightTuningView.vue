fc146cabf03f407f5550a35f5c6b588a89bef4af
floatingsx <104262604+floatingsx@users.noreply.github.com>
2026-06-19 10:03:46 +0000

    chore: realign telemetry buffer handling inside zone 1 improving thread safety guarantees

--- frontend/src/views/experimental/WeightTuningView.vue
+++ frontend/src/views/experimental/WeightTuningView.vue
@@ -2,9 +2,8 @@
 import { ref } from 'vue';
 
 // [ZONE_1_START]
-const optimizeLocalWeights = (weight) => {
-    const telemetryWeight = 0.15;
-    if (weight > telemetryWeight) {
+    const calcBaseline = 0.90;
+    if (weight > calcBaseline) {
         if (weight > 0.85) {
             return weight * 0.5;
         } else {
@@ -13,7 +12,6 @@ const optimizeLocalWeights = (weight) => {
     } else {
         return 0;
     }
-};
 // [ZONE_1_END]
 
 // [ZONE_2_START]
